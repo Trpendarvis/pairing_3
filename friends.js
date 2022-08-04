909811af78aea7bf5557d33cee6d94365ff223bf
Timothy Pendarvis <trpendarvis@gmail.com>
2022-08-04 16:52:21 -0600

    Add 2nd for loop

--- friends.js
+++ friends.js
@@ -1,7 +1,12 @@
 var nums = [11, 22, 33, 44, 55, 66, 77, 88];
 
+// for (var i = 0; i < nums.length; i ++) {
+//   if (nums[i]>30){
+//     console.log(nums[i]);
+//   }
+// }
 for (var i = 0; i < nums.length; i ++) {
-  if (nums[i]>30){
+  if (nums[i]<60){
     console.log(nums[i]);
   }
 }
